6423a2c2816c525ba9fceb5dbc43e515276d6cdd
joãoA <joaovictor.dudu03@gmail.com>
2025-08-19 21:13:57 -0300

    menu-hamburguer incluido. Falta pequenos ajustes finais

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
-console.log('hello world1')
-console.log('hello world2')
-console.log('hello world3')
-console.log('hello world4')
-console.log('hello world5')
+const openMenu = document.getElementById('openMenu')
+const closeMenu = document.getElementById('closeMenu')
+const mobileMenu = document.getElementById('mobileMenu')
+const body = document.body
+
+openMenu.addEventListener('click', function(){
+    mobileMenu.classList.toggle('active')
+     body.classList.add('no-scroll')
+})
+
+closeMenu.addEventListener('click', function(){
+    mobileMenu.classList.toggle('active')
+    body.classList.remove('no-scroll')
+})
